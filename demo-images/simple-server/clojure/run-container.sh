b4f59fa6d6b29f0ed1d1a7e45121570f70c85232
GabelBombe <dodomeki@gmail.com>
2019-08-13 09:10:02 -0700

    Clojure image

---
+++ demo-images/simple-server/clojure/run-container.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: ./run-container.sh <my-name> <my-tag>"
+    echo "Examples:"
+    echo "./run-container.sh karimarttila/simple-server-clojure-single-node 0.1"
+    echo "If you have problems starting the container, check it using sh:"
+    echo "  docker run -it --entrypoint /bin/bash karimarttila/simple-server-clojure-single-node:0.1"
+    echo "You can get a terminal to running container, use commands:"
+    echo "  docker ps -a => check the container name"
+    echo "  docker exec -it <container-name> sh"
+    echo "Stop and delete all containers:"
+    echo "  docker stop $(docker ps -aq) ; docker rm $(docker ps -aq)"
+    exit 1
+fi
+
+MY_NAME=$1
+MY_TAG=$2
+
+docker run -p 3045:3045 $MY_NAME:$MY_TAG
+
